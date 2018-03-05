dafbdb5159eb259bb953924be2a69bee1a5d9b59
Toms A <velo40@inbo.lv>
2018-03-05 12:53:05 +0000

    Added changes to mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,16 +6,24 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:components]
+      ContactMailer.contact_email(name,email,body).deliver
       flash[:success] = "Message sent."
       redirect_to new_contact_path
+    
     else
       flash[:danger] = @contact.errors.full_messages.join(",")
       redirect_to new_contact_path
     end
   end
+  
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :components)
     end
+  
+  
   
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
-  
   validates :email, presence: true
   validates :components, presence: true
 end
